93f54fb1a37df5d0d9d7d7bec1efa382ec8dac22
Sanzay Dahal <sanzaydahal12345@gmail.com>
2022-01-02 12:57:15 +1100

    Fetching Deployed Campaigns

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import factory from '../ethereum/factory';
 
-export default () => {
-  return <h1>This is the Campaign list page</h1>;
-};
+class CampaignIndex extends React.Component {
+  async componentDidMount() {
+    const campaigns = await factory.methods.getDeployedCampaigns().call();
+    console.log('Campaigns: ', campaigns);
+  }
+
+  render() {
+    return <h1>This is the Campaign list page</h1>;
+  }
+}
+export default CampaignIndex;
