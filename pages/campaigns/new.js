3b03acbdddd1eaa0d533ec80088b1bca8587c56d
Sanzay Dahal <sanzaydahal12345@gmail.com>
2022-01-02 17:01:21 +1100

    Form Error Handling

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
-import { Form, Button, Input } from 'semantic-ui-react';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
   state = {
     minimumContribution: '',
+    errorMessage: '',
   };
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await factory.methods
+        .createCampaign(this.state.minimumContribution)
+        .send({ from: accounts[0] });
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+  };
+
   render() {
     return (
       <Layout>
         <h3>Create a Campaign</h3>
-        <Form>
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
           <Form.Field>
             <label>Minimum Contribution</label>
             <Input
@@ -22,6 +38,7 @@ class CampaignNew extends Component {
               }
             />
           </Form.Field>
+          <Message error header='Oops!' content={this.state.errorMessage} />
           <Button primary>Create!</Button>
         </Form>
       </Layout>
