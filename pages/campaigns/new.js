48043761ce759bcfaaa7aecb6264e55f01776469
Sanzay Dahal <sanzaydahal12345@gmail.com>
2022-01-02 16:38:19 +1100

    Get started with Form

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import { Form, Button, Input } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 
 class CampaignNew extends Component {
+  state = {
+    minimumContribution: '',
+  };
   render() {
     return (
       <Layout>
-        <h1>New Campaign</h1>
+        <h3>Create a Campaign</h3>
+        <Form>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <Input
+              label='wei'
+              labelPosition='right'
+              value={this.state.minimumContribution}
+              onChange={(e) =>
+                this.setState({ minimumContribution: e.target.value })
+              }
+            />
+          </Form.Field>
+          <Button primary>Create!</Button>
+        </Form>
       </Layout>
     );
   }
