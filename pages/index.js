192deec4743d960ced8577128985db759d6c1de5
Sanzay Dahal <sanzaydahal12345@gmail.com>
2022-01-02 16:20:32 +1100

    Nested routing

--- pages/index.js
+++ pages/index.js
@@ -31,8 +31,13 @@ class CampaignIndex extends React.Component {
             href='https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css'
           />
           <h3>Open Campaigns</h3>
+          <Button
+            floated='right'
+            content='Create Campaign'
+            icon='add circle'
+            primary
+          />
           {this.renderCampaigns()}
-          <Button content='Create Campaign' icon='add circle' primary />
         </div>
       </Layout>
     );
