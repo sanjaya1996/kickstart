e4e5713ddea6461cfcf3d702a92c602497d7da36
Sanzay Dahal <sanzaydahal12345@gmail.com>
2022-01-03 23:03:40 +1100

    Create request form submit

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -6,18 +6,50 @@ import { Link, Router } from '../../../routes';
 import Layout from '../../../components/Layout';
 
 class RequestNew extends React.Component {
-  state = { value: '', description: '', recipient: '' };
+  state = {
+    value: '',
+    description: '',
+    recipient: '',
+    loading: false,
+    errorMessage: '',
+  };
 
   static async getInitialProps(props) {
     const { address } = props.query;
     return { address };
   }
 
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    const campaign = getCampaign(this.props.address);
+    const { description, value, recipient } = this.state;
+
+    this.setState({ loading: true, errorMessage: '' });
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+
+      await campaign.methods
+        .createRequest(description, web3.utils.toWei(value, 'ether'), recipient)
+        .send({ from: accounts[0] });
+
+      Router.pushRoute(`/campaigns/${this.props.address}/requests`);
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+
+    this.setState({ loading: false });
+  };
+
   render() {
     return (
       <Layout>
+        <Link route={`/campaigns/${this.props.address}/requests`}>
+          <a>Back</a>
+        </Link>
         <h3>Create a Request</h3>
-        <Form>
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
           <Form.Field>
             <label>Description</label>
             <Input
@@ -41,8 +73,10 @@ class RequestNew extends React.Component {
               onChange={(e) => this.setState({ recipient: e.target.value })}
             />
           </Form.Field>
-
-          <Button primary>Create!</Button>
+          <Message error header='Oops!' content={this.state.errorMessage} />
+          <Button loading={this.state.loading} primary>
+            Create!
+          </Button>
         </Form>
       </Layout>
     );
