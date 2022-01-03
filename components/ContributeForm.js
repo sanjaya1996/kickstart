938e6055674dff15f19ccb2dfae48d60791b3937
Sanzay Dahal <sanzaydahal12345@gmail.com>
2022-01-03 15:09:06 +1100

    Making a Contribution

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,15 +1,48 @@
 import React from 'react';
 import { Button, Form, Input, Message } from 'semantic-ui-react';
+import getCampaign from '../ethereum/campaign';
+import web3 from '../ethereum/web3';
+import { Router } from '../routes';
 
 class ContributeForm extends React.Component {
+  state = { value: '', loading: false, errorMessage: '' };
+
+  onSubmit = async (e) => {
+    e.preventDefault();
+    this.setState({ loading: true, errorMessage: '' });
+    const campaign = getCampaign(this.props.address);
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await campaign.methods.contribute().send({
+        from: accounts[0],
+        value: web3.utils.toWei(this.state.value, 'ether'),
+      });
+
+      Router.replaceRoute(`/campaign/${this.props.address}`);
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+
+    this.setState({ loading: false });
+  };
+
   render() {
     return (
-      <Form>
+      <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
         <Form.Field>
           <label>Amount to Contribute</label>
-          <Input label='ether' labelPosition='right' />
+          <Input
+            value={this.state.value}
+            onChange={(e) => this.setState({ value: e.target.value })}
+            label='ether'
+            labelPosition='right'
+          />
         </Form.Field>
-        <Button primary>Contribute!</Button>
+        <Message error header='Oops!' content={this.state.errorMessage} />
+        <Button loading={this.state.loading} primary>
+          Contribute!
+        </Button>
       </Form>
     );
   }
--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -11,6 +11,7 @@ class CampaignShow extends React.Component {
     const summary = await campaign.methods.getSummary().call();
 
     return {
+      address: props.query.address,
       minimumContribution: summary[0],
       balance: summary[1],
       requestsCount: summary[2],
@@ -72,7 +73,7 @@ class CampaignShow extends React.Component {
         <Grid>
           <Grid.Column width={10}>{this.renderCards()}</Grid.Column>
           <Grid.Column width={6}>
-            <ContributeForm />
+            <ContributeForm address={this.props.address} />
           </Grid.Column>
         </Grid>
       </Layout>
