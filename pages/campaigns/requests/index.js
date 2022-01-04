f39bc4081f6030b31c3f55c56b399ffb4db793da
Sanzay Dahal <sanzaydahal12345@gmail.com>
2022-01-04 22:58:50 +1100

    Rendering a table

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Table } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Layout from '../../../components/Layout';
 import getCampaign from '../../../ethereum/campaign';
+import RequestRow from '../../../components/RequestRow';
 
 class RequestIndex extends React.Component {
   static async getInitialProps(props) {
@@ -20,7 +21,15 @@ class RequestIndex extends React.Component {
     return { address, requests, requestCount };
   }
 
+  renderRows = () => {
+    return this.props.requests.map((request, index) => (
+      <RequestRow key={index} request={request} address={this.props.address} />
+    ));
+  };
+
   render() {
+    const { Header, Row, HeaderCell, Body } = Table;
+
     return (
       <Layout>
         <h3>Requests</h3>
@@ -29,6 +38,20 @@ class RequestIndex extends React.Component {
             <Button primary>Add Request</Button>
           </a>
         </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+          <Body>{this.renderRows()}</Body>
+        </Table>
       </Layout>
     );
   }
