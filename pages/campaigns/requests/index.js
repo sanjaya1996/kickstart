11e3f1983e4c99db43e15a34d4ce0cdf5287b9f1
Sanzay Dahal <sanzaydahal12345@gmail.com>
2022-01-04 22:49:00 +1100

    Fetch requests one by one using Promise.all and requestCount

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { Button } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Layout from '../../../components/Layout';
+import getCampaign from '../../../ethereum/campaign';
 
 class RequestIndex extends React.Component {
   static async getInitialProps(props) {
     const { address } = props.query;
-    return { address };
+    const campaign = getCampaign(address);
+
+    const requestCount = await campaign.methods.getRequestsCount().call();
+
+    const requests = await Promise.all(
+      [...Array(parseInt(requestCount)).keys()].map((_, index) =>
+        campaign.methods.requests(index).call()
+      )
+    );
+
+    return { address, requests, requestCount };
   }
 
   render() {
