780afd9906d0334fb4aa934b4a9216d543318ba8
Sanzay Dahal <sanzaydahal12345@gmail.com>
2022-01-04 23:30:42 +1100

    Approving and finalizing request

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import { Table } from 'semantic-ui-react';
+import { Table, Button } from 'semantic-ui-react';
 import web3 from '../ethereum/web3';
+import getCampaign from '../ethereum/campaign';
 
 class RequestRow extends React.Component {
+  onApprove = async () => {
+    const campaign = getCampaign(this.props.address);
+
+    const accounts = await web3.eth.getAccounts();
+    await campaign.methods
+      .approveRequest(this.props.id)
+      .send({ from: accounts[0] });
+  };
+
+  onFinalize = async () => {
+    const campaign = getCampaign(this.props.address);
+
+    const accounts = await web3.eth.getAccounts();
+    await campaign.methods
+      .finalizeRequest(this.props.id)
+      .send({ from: accounts[0] });
+  };
+
   render() {
     const { Row, Cell } = Table;
     const { id, request, address, approversCount } = this.props;
@@ -15,6 +34,16 @@ class RequestRow extends React.Component {
         <Cell>{request.recipient}</Cell>
         <Cell>
           {request.approvalCount}/{approversCount}
+        </Cell>
+        <Cell>
+          <Button color='green' basic onClick={this.onApprove}>
+            Approve
+          </Button>
+        </Cell>
+        <Cell>
+          <Button color='teal' basic onClick={this.onFinalize}>
+            Finalize
+          </Button>
         </Cell>
       </Row>
     );
